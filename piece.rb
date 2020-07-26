d3bfb9363535a45479af81d66f6fb99d49507869
Kian <Ksadeghi@sas.upenn.edu>
2020-07-26 15:13:50 -0400

    initializer complete

---
+++ piece.rb
@@ -0,0 +1,27 @@
+class Piece
+
+  @@num_of_pieces = 0
+
+  attr_accessor :x_pos, :y_pos, :type
+
+  def initialize(x_pos, y_pos, type)
+    @x_pos = x_pos
+    @y_pos = y_pos
+    @type = type
+    @@num_of_pieces += 1
+  end
+
+  def self.number_of_pieces
+    @@num_of_pieces
+  end
+
+  def draw
+    if @type == 1
+      puts "X"
+    else 
+      puts "O"      
+    end
+  end
+
+
+end
